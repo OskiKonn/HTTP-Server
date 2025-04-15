eee9c2895b8bd04f925a822f9671b5fdf70848c6
OskiKonn <oskarej@o2.pl>
2025-04-16 01:25:10 +0200

    added extracting url path

--- src/main.c
+++ src/main.c
@@ -7,19 +7,18 @@
 #include <errno.h>
 #include <unistd.h>
 
+int handleRequest(int);
+
 int main() {
 	// Disable output buffering
 	setbuf(stdout, NULL);
  	setbuf(stderr, NULL);
 
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	printf("Logs from your program will appear here!\n");
-
-	// Uncomment this block to pass the first stage
-	//
+	// client structure, socket file descriptor
 	int server_fd, client_addr_len;
 	struct sockaddr_in client_addr;
 	
+	// Create new TCP socket
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
 		printf("Socket creation failed: %s...\n", strerror(errno));
@@ -34,11 +33,13 @@ int main() {
 		return 1;
 	}
 	
+	// Set server socket to be IPv4 address on port 4221 listening to all messages
 	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
 									 .sin_port = htons(4221),
 									 .sin_addr = { htonl(INADDR_ANY) },
 									};
 	
+
 	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
 		printf("Bind failed: %s \n", strerror(errno));
 		return 1;
@@ -61,19 +62,78 @@ int main() {
 		printf("Error handling new connection: %s \n", strerror(errno));
 		return 1;
 	}
-	
-	char *buff = "HTTP/1.1 200 OK\r\n\r\n";
-	int sent = send(new_fd, buff, strlen(buff), 0);
 
-	if (sent < 0)
+	char* buff;
+	int bytes_sent;
+
+	if (handleRequest(new_fd) != 0)
+		buff = "HTTP/1.1 404 Not Found\r\n\r\n";
+	else
+		buff = "HTTP/1.1 200 OK\r\n\r\n";
+		
+
+	bytes_sent = send(new_fd, buff, strlen(buff), 0);
+		
+	if (bytes_sent < 0)
 	{
 		printf("Failed sendind message: %s", strerror(errno));
 		return 1;
 	}
+	
+	/*
+	char *buff = "HTTP/1.1 200 OK\r\n\r\n";
+	int bytes_sent = send(new_fd, buff, strlen(buff), 0);
+
+	if (bytes_sent < 0)
+	{
+		printf("Failed sendind message: %s", strerror(errno));
+		return 1;
+	}
+	*/
 
 	printf("Message sent!\n");
 
 	close(server_fd);
+
+	return 0;
+}
+
+int handleRequest(int new_fd)
+{
+	char buff[256], target[128];
+	char* token;
+	size_t i = 0;
+	ssize_t bytes_recvd = recv(new_fd, buff, 256, 0);
+	
+	if (bytes_recvd == -1)
+	{
+		printf("Error reading message: %s", strerror(errno));
+		return -1;
+	}
+
+	token = strchr(buff, '/');
+
+	if (token == NULL)
+	{
+		printf("Invalid request, no URL");
+		return -1;
+	}
+
+	while (*token != ' ')
+	{
+		target[i] = *token;
+		token++;
+		i++;
+	}
+
+	target[i] = '\0';
+	printf("Message: %s :: length : %d\n\n", target, strlen(target));
+
+	if (strcmp(target, "/") != 0)
+	{
+		printf("Invalid URL: %s", target);
+		return -1;
+	}
 
 	return 0;
 }
