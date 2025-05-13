0ef83e910731f5513bcb56553c35f8ec656756d3
OskiKonn <oskarej@o2.pl>
2025-05-13 12:50:11 +0200

    Removed unnecesary files and edited build files

---
+++ build.sh
@@ -0,0 +1,10 @@
+
+set -e
+
+(
+  cd "$(dirname "$0")"
+  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake --build ./build
+)
+
+exec $(dirname $0)/build/http-server "$@"
